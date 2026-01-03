4f8a64390cae47db0dc9184542736257d55e0513
Ali Moradpour <mr.mrdp@yahoo.com>
2026-01-03 18:15:19 +0330

    add half of Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,23 @@
+import { allCocktails } from "../../constants/index";
+
+const Menu = () => {
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+     
+     <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf" />
+     <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf" />
+
+     <h2 id="menu-heading" className="sr-only">Cocktail Menu</h2>
+
+     <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+     {allCocktails.map(()=> (
+      <div>
+       
+      </div>
+     ))}
+     </nav>
+    </section>
+  )
+}
+
+export default Menu
